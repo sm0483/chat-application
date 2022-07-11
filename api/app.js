c6badc147501acf4288abe20cd058e48b7ebf0d1
Sreerag <msratofficial@gmail.com>
2022-07-11 23:17:17 +0530

    build:set up json responce for bad request

--- api/app.js
+++ api/app.js
@@ -1,17 +1,10 @@
 const express= require('express');
-const http=require('http');
 const app=express();
-const server=http.createServer(app);
-const {Server}=require('socket.io');
-const io=new Server(server);
-const path=require('path');
 require('dotenv').config();
-const flashMiddleware=require('./middleware/flash');
-
 const pageNotFound=require('./middleware/not-found');
 //flash message
 
-const flash=require('connect-flash');
+
 //use of cookiparser
 const cookieParser = require('cookie-parser');
 
@@ -24,12 +17,7 @@ const User=require('./models/user');
 
 
 
-// to read body from html page
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static("/home/user/Desktop/project/real-time-chat-app/views"));
-
+// to read json request
 app.use(express.json());
 const connectDb=require('./db/connect');
 const errorHandler=require('./middleware/err');
@@ -50,13 +38,13 @@ app.use(
     })
   );
 
-app.use(flash());
-//pasport-local
+
+  //for auth
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(flashMiddleware);
+
 
 
 
@@ -69,21 +57,12 @@ passport.deserializeUser(User.deserializeUser());
 
 
 
-app.set('view engine','ejs');
-app.set('views','/home/user/Desktop/project/real-time-chat-app/views/html');
-io.on('connection',(socket)=>{
-    console.log('a user connected');
-
-})
-
-
-
 app.use('/chatapp',userRoute);
 
 const port=process.env.PORT || 3000;
 
-server.listen(port,()=>{
 
+app.listen(port,()=>{
     console.log('sever running----');
 })
 
--- api/middleware/not-found.js
+++ api/middleware/not-found.js
@@ -1,5 +1,13 @@
+const {
+    StatusCodes,
+    getReasonPhrase
+}=require('http-status-codes');
+
 const pageNotFound=(req,res,next)=>{
-    res.status(404).render('not-found');
+    res.status(StatusCodes.BAD_REQUEST).json({
+        "message":getReasonPhrase(StatusCodes.BAD_REQUEST),
+        "statusCode":StatusCodes.BAD_REQUEST
+    })
 }
 
 module.exports=pageNotFound;
