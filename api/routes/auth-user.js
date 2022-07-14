552d9a3431d93c18f8a34f8184647e9f159bae9b
Sreerag <msratofficial@gmail.com>
2022-07-14 20:02:36 +0530

    fix: renamed:    ../jModels/user-register.js -> ../jModels/user.js

--- api/routes/auth-user.js
+++ api/routes/auth-user.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
-const userValidate=require('../middleware/userValidate');
-const loginValidate=require('../middleware/loginValidate');
+const {userValidate}=require('../middleware/userValidate');
+const {loginValidate}=require('../middleware/loginValidate');
 
 
 const {
