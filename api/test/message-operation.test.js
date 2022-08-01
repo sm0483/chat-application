4cbc3f9680ca2f8e78494bb9adb8e3430241a2e2
Sreerag <msratofficial@gmail.com>
2022-08-01 22:50:56 +0530

    test: add test function to interact with message schema

--- api/test/message-operation.test.js
+++ api/test/message-operation.test.js
@@ -1,8 +1,7 @@
 const { 
     createMessageDb,
     deleteMessageDb,
-    getMessageByUserid,
-    getMessageDbByMessageId,
+    getAllMessageDb,
     clearDb
 }=require('../db/message-operation');
 
@@ -10,15 +9,20 @@ const {connectDb,closeDb}=require('../db/connect');
 
 require('dotenv').config();
 
-const {v4 : uuidv4} = require('uuid');
-const { deleteUserDb } = require('../db/db-operation');
-const { response } = require('express');
+const mongoose=require('mongoose');
+const { EXPECTATION_FAILED } = require('http-status-codes');
 
-const userOne=uuidv4();
-const userTwo=uuidv4();
-const userThree=uuidv4();
-const userFour=uuidv4();
+const getObj=(str)=>{ //string ->object
+    return mongoose.Types.ObjectId(str);
+}
 
+
+
+const userOne="62d78afd4e5a0f1a955f9f0d";
+const userTwo="62d78afd4e5a0f1a955f9f0e";
+
+const messageIdOne="62d78afd4e5a0f1a955f9f0f";
+const messageIdTwo="62k78afd4e5a0f1a955f9f0d";
 
 beforeAll(async()=>{
     const connection=await connectDb(process.env.URL);
@@ -30,109 +34,58 @@ afterAll(async()=>{
 })
 
 const testMessage={
-    senderId:userOne,
-    reciverId:userTwo,
+    senderId:getObj(userOne),
+    contactId:getObj(messageIdOne),
     message:"Hello how are you"
 }
 
-const testMessage1={
-    senderId:userTwo,
-    reciverId:userOne,
-    message:"hello world"
-}
-
-const testMessage2={
-    senderId:userTwo,
-    reciverId:userThree,
-    message:"userTwo->userThree"
-}
-
-const testMessage3={
-    senderId:userTwo,
-    reciverId:userThree,
-    message:"userTwo->userThree"
+const testMessageOne={
+    senderId:getObj(userOne),
+    contactId:getObj(messageIdOne),
+    message:"Hello"
 }
 
 
-const testMessage4={
-    senderId:userThree,
-    reciverId:userTwo,
-    message:"userThree->userTwo"
 
-}
 
-let messageId=undefined;
-let messageId1=undefined;
 
 describe("Crud operation on message model",()=>{
     test("create message",async()=>{
         const response=await createMessageDb(testMessage);
+        expect(response.contactId).toBe(testMessage.contactId);
         expect(response.senderId).toBe(testMessage.senderId);
-        expect(response.reciverId).toBe(testMessage.reciverId);
         expect(response.message).toBe(testMessage.message);
-        messageId=response._id;
     })
 
-    test("create document second message",async()=>{
-        const response=await createMessageDb(testMessage1);
-        expect(response.senderId).toBe(testMessage1.senderId);
-        expect(response.reciverId).toBe(testMessage1.reciverId);
-        expect(response.message).toBe(testMessage1.message);
-        messageId1=response._id;
- 
-    })
-
-    test("create document third message",async()=>{
-        const response=await createMessageDb(testMessage2);
-        expect(response.senderId).toBe(testMessage2.senderId);
-        expect(response.reciverId).toBe(testMessage2.reciverId);
-        expect(response.message).toBe(testMessage2.message);
- 
-    })
-
-    test("create document fourth message",async()=>{
-        const response=await createMessageDb(testMessage3);
-        expect(response.senderId).toBe(testMessage3.senderId);
-        expect(response.reciverId).toBe(testMessage3.reciverId);
-        expect(response.message).toBe(testMessage3.message);
- 
-    })
-
-    test("create document fifth message",async()=>{
-        const response=await createMessageDb(testMessage4);
-        expect(response.senderId).toBe(testMessage4.senderId);
-        expect(response.reciverId).toBe(testMessage4.reciverId);
-        expect(response.message).toBe(testMessage4.message);
- 
-    })
-
-    test("fetch message data with MessageId",async()=>{
-        const response=await getMessageDbByMessageId(messageId1);
-        expect(response.senderId).toBe(testMessage1.senderId);
-        expect(response.reciverId).toBe(testMessage1.reciverId);
-        expect(response.message).toBe(testMessage1.message);
-   
-    })
-
-    test("fetch message with both userId",async()=>{
-        const response=await getMessageByUserid(userTwo,userThree);
-        expect(response.length).toBe(3);
-        expect(response[0].senderId).toBe(userTwo);
-        expect(response[0].reciverId).toBe(userThree);
-        expect(response[1].senderId).toBe(userTwo);
-        expect(response[1].reciverId).toBe(userThree);
-        expect(response[2].senderId).toBe(userThree);
-        expect(response[2].reciverId).toBe(userTwo);
+    test("create message second",async()=>{
+        const response=await createMessageDb(testMessageOne);
+        expect(response.contactId).toBe(testMessageOne.contactId);
+        expect(response.senderId).toBe(testMessageOne.senderId);
+        expect(response.message).toBe(testMessageOne.message);
 
     })
 
-    test("delete message ",async()=>{
-        const response=await deleteMessageDb(messageId);
-        expect(response.senderId).toBe(testMessage.senderId);
-        expect(response.reciverId).toBe(testMessage.reciverId);
-        expect(response.message).toBe(testMessage.message);
+    test("get  message data",async()=>{
+        const data={
+            senderId:testMessage.senderId,
+            contactId:testMessage.contactId
+        }
+        const response=await getAllMessageDb(data);
+        expect(JSON.stringify(response[0].contactId)).toBe(JSON.stringify(testMessage.contactId));
+        expect(JSON.stringify(response[0].senderId)).toBe(JSON.stringify(testMessage.senderId));
+        expect(response.length).toBe(2);
 
+    })
+
+    test("get message with only messageId",async()=>{
+        const data={
+            contactId:testMessage.contactId
+        }
+        const response=await getAllMessageDb(data);
+        expect(response.length).toBe(0);
     })
 
     
 })
+
+
